264493b9da4aaf26bef6d6c5daf04e95c10554cc
cxxxr <g23tlm@gmail.com>
2021-05-22 22:32:01 +0900

    indent -kr -nut -i4

--- MyLoaderPkg/Main.c
+++ MyLoaderPkg/Main.c
@@ -7,22 +7,22 @@
 #include  <Protocol/DiskIo2.h>
 #include  <Protocol/BlockIo.h>
 
-void save_memmap(EFI_FILE_PROTOCOL* root_dir,
+void save_memmap(EFI_FILE_PROTOCOL * root_dir,
                  UINTN memory_map_size,
-                 VOID* memory_map,
-                 UINTN map_descriptor_size)
+                 VOID * memory_map, UINTN map_descriptor_size)
 {
 
-    EFI_FILE_PROTOCOL* memmap_file;
+    EFI_FILE_PROTOCOL *memmap_file;
 
     root_dir->Open(root_dir,
                    &memmap_file,
                    L"\\memmap",
-                   EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE,
-                   0);
+                   EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE |
+                   EFI_FILE_MODE_CREATE, 0);
 
     {
-        CHAR8* header = "Index, Type, PhysicalStart, NumberOfPages, Attribute\n";
+        CHAR8 *header =
+            "Index, Type, PhysicalStart, NumberOfPages, Attribute\n";
         UINTN len = AsciiStrLen(header);
         memmap_file->Write(memmap_file, &len, header);
 
@@ -30,14 +30,15 @@ void save_memmap(EFI_FILE_PROTOCOL* root_dir,
 
         EFI_PHYSICAL_ADDRESS iter;
         int i;
-        for (iter = (EFI_PHYSICAL_ADDRESS)memory_map, i = 0;
-             iter < (EFI_PHYSICAL_ADDRESS)memory_map + memory_map_size;
+        for (iter = (EFI_PHYSICAL_ADDRESS) memory_map, i = 0;
+             iter < (EFI_PHYSICAL_ADDRESS) memory_map + memory_map_size;
              iter += map_descriptor_size, i++) {
-            EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)iter;
+            EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR *) iter;
             UINTN len = AsciiSPrint(buf,
                                     sizeof(buf),
                                     "%u, %x, %08lx, %lx, %lx\n",
-                                    i, desc->Type, desc->PhysicalStart, desc->NumberOfPages,
+                                    i, desc->Type, desc->PhysicalStart,
+                                    desc->NumberOfPages,
                                     desc->Attribute & 0xffffflu);
             memmap_file->Write(memmap_file, &len, buf);
         }
@@ -46,57 +47,51 @@ void save_memmap(EFI_FILE_PROTOCOL* root_dir,
     memmap_file->Close(memmap_file);
 }
 
-void open_root_dir(EFI_HANDLE image_handle,
-                   EFI_FILE_PROTOCOL** root_dir)
+void open_root_dir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL ** root_dir)
 {
-    EFI_LOADED_IMAGE_PROTOCOL* loaded_image;
-    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* fs;
+    EFI_LOADED_IMAGE_PROTOCOL *loaded_image;
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *fs;
 
     gBS->OpenProtocol(image_handle,
                       &gEfiLoadedImageProtocolGuid,
-                      (VOID**)&loaded_image,
+                      (VOID **) & loaded_image,
                       image_handle,
-                      NULL,
-                      EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+                      NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
 
     gBS->OpenProtocol(loaded_image->DeviceHandle,
                       &gEfiSimpleFileSystemProtocolGuid,
-                      (VOID**)&fs,
+                      (VOID **) & fs,
                       image_handle,
-                      NULL,
-                      EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+                      NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
 
     fs->OpenVolume(fs, root_dir);
 }
 
 EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
-                           EFI_SYSTEM_TABLE *system_table)
+                           EFI_SYSTEM_TABLE * system_table)
 {
     CHAR8 memory_map_buffer[1024 * 16];
 
     UINTN memory_map_size = sizeof(memory_map_buffer);
-    VOID* memory_map = memory_map_buffer;
+    VOID *memory_map = memory_map_buffer;
     UINTN map_key;
     UINTN map_descriptor_size;
     UINT32 map_descriptor_version;
 
     gBS->GetMemoryMap(&memory_map_size,
-                      (EFI_MEMORY_DESCRIPTOR*)memory_map,
+                      (EFI_MEMORY_DESCRIPTOR *) memory_map,
                       &map_key,
-                      &map_descriptor_size,
-                      &map_descriptor_version);
+                      &map_descriptor_size, &map_descriptor_version);
 
     Print(L"memory_map_buffer = %08lx, memory_map_size = %08lx\n",
           memory_map_buffer, memory_map_size);
 
-    EFI_FILE_PROTOCOL* root_dir;
+    EFI_FILE_PROTOCOL *root_dir;
 
     open_root_dir(image_handle, &root_dir);
 
     save_memmap(root_dir,
-                memory_map_size,
-                memory_map,
-                map_descriptor_size);
+                memory_map_size, memory_map, map_descriptor_size);
 
     Print(L"Hello World");
     while (1);
