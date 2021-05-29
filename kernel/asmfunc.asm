3614f8a8ef95e53753ef498bc9c14b1777af33a6
cxxxr <g23tlm@gmail.com>
2021-05-29 22:51:36 +0900

    make.shにasmfunc.asmを追加

--- kernel/asmfunc.asm
+++ kernel/asmfunc.asm
@@ -1,6 +1,13 @@
 bits 64
 section .text
 
+; rdi, rsi
+
+; rax, eax, ax, ah:al
+; rsi, esi, si, sil
+; rcx, ecx, cx, ch:cl
+
+; void out8(uint16 addr, uint8 data)
 global out8
 out8:
 	mov dx, di
@@ -8,6 +15,7 @@ out8:
 	out dx, al
 	ret
 
+; uint8 in8(uint16 addr)
 global in8
 in8:
 	mov edx, ecx
--- kernel/make.sh
+++ kernel/make.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/sh -x
 
 clang -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -c main.c
+nasm -f elf64 -o asmfunc.o asmfunc.asm
 ld.lld --entry KernelMain -z norelro --image-base 0x100000 --static -o kernel.elf main.o asmfunc.o
