883f014da7e81f4e80cd9ba128c81a4b7f2eb6af
cxxxr <g23tlm@gmail.com>
2021-06-06 23:30:15 +0900

    空きをフリーリストで管理するK&R mallocの方法に書き直し

--- kernel/memory.c
+++ kernel/memory.c
@@ -2,36 +2,9 @@
 #include "efi.h"
 #include "serial.h"
 
-// ページングで扱う最大サイズと合わせて64GiBにする
-static const uint64 max_physical_memory_size = 64 * 1024 * 1024 * 1024;
-static const uint64 frame_size = 4 * 1024;
-static const uint64 memory_map_size = max_physical_memory_size / frame_size;
 
-static uint64 memory_map[memory_map_size];
 
-static void set_bit(uint64 pos, int bit)
-{
-    uint64 line = pos / 64;
-    uint64 column = pos % 64;
 
-    if (bit) {
-        memory_map[line] |= ((uint64)1 << column);
-    } else {
-        memory_map[line] &= ~((uint64)1 << column);
-    }
-}
-
-static int get_bit(uint64 line, uint64 column)
-{
-    return (memory_map[line] >> column) & 1;
-}
-
-static void mark_free(uint64 start, uint64 size)
-{
-    for (uint64 i = 0; i < size; i++) {
-        set_bit(start + i, 1);
-    }
-}
 
 static int is_available_memory_type(_EFI_MEMORY_TYPE memory_type)
 {
@@ -45,7 +18,53 @@ static int is_available_memory_type(_EFI_MEMORY_TYPE memory_type)
     }
 }
 
-#define KiB(n) ((n) * 1024)
+
+
+struct page {
+    struct page *next;
+    uint64 size;
+};
+
+static struct page head;
+
+// 物理アドレス0とNULLの区別が付かないため、最後の要素の1ビット目に印をつける
+static int is_end(struct page* p)
+{
+    return (uint64)p->next & 1;
+}
+
+static void set_end(struct page* p)
+{
+    p->next = (struct page*)((uint64)p->next | 1);
+}
+
+static uint8 is_first = 1;
+static void append_free(uint64 start, uint64 size)
+{
+    struct page *node = (struct page *)start;
+    node->next = head.next;
+    node->size = size * 4096; // REVIEW: sizeはヘッダ分減らさなくて良いのか
+
+    if (is_first) {
+        is_first = 0;
+        set_end(node);
+    }
+
+    head.next = node;
+}
+
+void print_memory(void)
+{
+    for (struct page *p = head.next; ; p = p->next) {
+        print_uint64((uint64)p);
+        print_char(' ');
+        print_uint64(p->size);
+        print_char(' ');
+        print_uint64(p->size / 4096);
+        print_char('\n');
+        if (is_end(p)) break;
+    }
+}
 
 void init_memory(MemoryMap* map)
 {
@@ -54,8 +73,10 @@ void init_memory(MemoryMap* map)
          iter += map->descriptor_size) {
         _EFI_MEMORY_DESCRIPTOR *desc = (_EFI_MEMORY_DESCRIPTOR*)iter;
         if (is_available_memory_type(desc->type)) {
-            mark_free(desc->physical_start / KiB(4),
-                      desc->number_of_pages);
+            append_free(desc->physical_start,
+                        desc->number_of_pages);
         }
     }
+
+    print_memory();
 }
