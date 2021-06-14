d4de350466b36c5b9211d334c109a497458653e1
cxxxr <g23tlm@gmail.com>
2021-06-14 21:31:15 +0900

    ボリューム名が8文字を越えてておかしくなるので削除

--- userland/make.sh
+++ userland/make.sh
@@ -2,7 +2,7 @@
 
 rm fat_disk
 dd if=/dev/zero of=fat_disk bs=1M count=1
-mkfs.fat -n 'FILESYSTEM' -s 2 -f 2 -R 32 -F 32 fat_disk
+mkfs.fat -s 2 -f 2 -R 32 -F 32 fat_disk
 
 mkdir mnt
 sudo mount -o loop fat_disk mnt
