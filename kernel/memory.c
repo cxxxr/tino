81a9d21191bf0b8fa04196ed4d4b3c11b521e824
cxxxr <g23tlm@gmail.com>
2021-07-10 18:41:17 +0900

    kernel/efi.hを削除しloader/efi.hを使うようにする

--- kernel/memory.c
+++ kernel/memory.c
@@ -1,12 +1,12 @@
 #include "memory.h"
-#include "efi.h"
+#include "../loader/efi.h"
 #include "serial.h"
 
-static int is_available_memory_type(_EFI_MEMORY_TYPE memory_type) {
+static int is_available_memory_type(EFI_MEMORY_TYPE memory_type) {
   switch (memory_type) {
-  case _EFI_BOOT_SERVICES_CODE:
-  case _EFI_BOOT_SERVICES_DATA:
-  case _EFI_CONVENTIONAL_MEMORY:
+  case EFI_BOOT_SERVICES_CODE:
+  case EFI_BOOT_SERVICES_DATA:
+  case EFI_CONVENTIONAL_MEMORY:
     return 1;
   default:
     return 0;
@@ -84,7 +84,7 @@ static void print_memory_map(MemoryMap *map) {
   print_string("type physical_start virtual_start number_of_pages attribute\n");
   for (uint64 iter = (uint64)map->base; iter < (uint64)(map->base + map->size);
        iter += map->descriptor_size) {
-    _EFI_MEMORY_DESCRIPTOR *desc = (_EFI_MEMORY_DESCRIPTOR *)iter;
+    EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR *)iter;
     print_char(is_available_memory_type(desc->type) ? '!' : ' ');
     print_uint64_with_padding(desc->type, 4);
     print_char(' ');
@@ -104,7 +104,7 @@ void init_memory(MemoryMap *map) {
 
   for (uint64 iter = (uint64)map->base; iter < (uint64)(map->base + map->size);
        iter += map->descriptor_size) {
-    _EFI_MEMORY_DESCRIPTOR *desc = (_EFI_MEMORY_DESCRIPTOR *)iter;
+    EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR *)iter;
     if (is_available_memory_type(desc->type)) {
       append_free(desc->physical_start, desc->number_of_pages);
     }
