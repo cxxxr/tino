d317367c7ee0fc37f30c2a60daa9ed99733887b5
cxxxr <g23tlm@gmail.com>
2021-07-18 00:54:03 +0900

    ユーザープログラムを0xffff8000....に仮想アドレスを配置する

--- userland/hello/make.sh
+++ userland/hello/make.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 
-clang -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti hello.c -c
-ld.lld --entry main -z norelro --image-base 0 --static -o hello hello.o
+clang -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -mcmodel=large hello.c -c
+ld.lld --entry main -z norelro --image-base 0 --static --image-base 0xffff800000000000 -o hello hello.o
