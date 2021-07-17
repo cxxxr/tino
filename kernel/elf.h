5c1d7eea22001c4ffaa1ca7b08f5a157af53f5c2
cxxxr <g23tlm@gmail.com>
2021-07-18 00:35:05 +0900

    elfのET_定数の定義を追加

--- kernel/elf.h
+++ kernel/elf.h
@@ -53,4 +53,14 @@ typedef struct {
 #define PT_LOPROC 0x70000000
 #define PT_HIPROC 0x7fffffff
 
+#define ET_NONE 0
+#define ET_REL 1
+#define ET_EXEC 2
+#define ET_DYN 3
+#define ET_CORE 4
+#define ET_LOOS 0xfe00
+#define ET_HIOS 0xfeff
+#define ET_LOPROC 0xff00
+#define ET_HIPROC 0xffff
+
 #endif
